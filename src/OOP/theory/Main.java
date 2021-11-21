0b8ccb9168e8d5bb9d2e367c06c9e79df0b27138
FARDIN98 <fardinahmed66@gmail.com>
2021-11-21 11:19:34 +0600

    codeed in study room

---
+++ src/OOP/theory/Main.java
@@ -0,0 +1,19 @@
+package OOP.theory;
+
+class Student{
+    String name;
+    int id;
+
+    //Default constructor
+//    public Student(){
+//
+//    }
+
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+        Student st = new Student();
+    }
+}
