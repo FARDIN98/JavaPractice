662e0d36f08a0e9d03c0c7c610d05df57ff844c3
FARDIN98 <fardinahmed66@gmail.com>
2021-11-29 17:37:15 +0600

    changes

--- src/OOP/lab/Employee.java
+++ src/OOP/lab/Employee.java
@@ -1,3 +1,9 @@
+/*Write a program that would print the information (name, year of joining, salary, address) of three employees by creating a class named 'Employee'. The output should be as follows:
+        Name        Year of joining        Address
+        Robert            1994                64C- WallsStreat
+        Sam                2000                68D- WallsStreat
+        John                1999                26B- WallsStreat*/
+
 package OOP.lab;
 import java.util.Scanner;
 class Employee {
