88e098a7fbcaa0733f8a86cf5c74af95f02e2d03
FARDIN98 <fardinahmed66@gmail.com>
2021-11-30 06:23:41 +0600

    changes

---
+++ src/theory1.java
@@ -0,0 +1,20 @@
+class Student{
+    String name;
+    int id;
+
+    public Student(String n, int i){
+        name = n;
+        id  = i;
+    }
+}
+
+public class theory1 {
+    public static void main(String[] args) {
+//        Student s1 = new Student();
+        Student s2 = new Student("Fardin", 21);
+//        System.out.println(s1.name + " " + s1.id);
+        System.out.println(s2.name);
+    }
+
+
+}
