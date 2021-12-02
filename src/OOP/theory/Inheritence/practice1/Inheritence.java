9b130435b6a3867c1cc42dc662b816433b001b2d
FARDIN98 <fardinahmed66@gmail.com>
2021-12-02 20:01:52 +0600

    super class

--- src/OOP/theory/Inheritence/practice1/Inheritence.java
+++ src/OOP/theory/Inheritence/practice1/Inheritence.java
@@ -1,10 +1,15 @@
 package OOP.theory.Inheritence.practice1;
 
 public class Inheritence {
+//    public static void main(String[] args) {
+//        Child example = new Child();
+//        example.childMethod();
+//        example.parentMethod();
+//        System.out.println(example.parentVariable);
+//    }
     public static void main(String[] args) {
-        Child example = new Child();
-        example.childMethod();
-        example.parentMethod();
-        System.out.println(example.parentVariable);
+        Child s = new Child("Parent", "Child");
+        s.show();
+        s.display();
     }
 }
--- src/OOP/theory/Inheritence/practice1/Parent.java
+++ src/OOP/theory/Inheritence/practice1/Parent.java
@@ -1,8 +1,12 @@
 package OOP.theory.Inheritence.practice1;
 
 public class Parent {
-    public int parentVariable = 10;
-    public void parentMethod(){
-        System.out.println("This is Parent Method");
+//    private int parentVariable = 10;
+//    public void parentMethod(){
+//        System.out.println("parent variable: " + parentVariable);
+//    }
+    String name;
+    void message(){
+        System.out.println("Welcome to Parent class");
     }
 }
