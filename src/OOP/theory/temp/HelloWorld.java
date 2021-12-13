f9e866c55db0c2f06ac6b9b694adbad03522dea3
FARDIN98 <fardinahmed66@gmail.com>
2021-12-13 16:28:58 +0600

    changes

---
+++ src/OOP/theory/temp/HelloWorld.java
@@ -0,0 +1,7 @@
+package OOP.theory.temp;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        test a = new B();
+    }
+}
---
+++ src/OOP/theory/temp/test.java
@@ -0,0 +1,12 @@
+package OOP.theory.temp;
+
+ class test {
+    test(){
+        System.out.print("Hello ");
+    }
+}
+ class B extends test{
+    B(){
+        System.out.println("World");
+    }
+ }
