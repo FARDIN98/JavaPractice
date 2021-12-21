86c33eaa7d4d244af2624b69435dfa4c2d83b98d
FARDIN98 <fardinahmed66@gmail.com>
2021-12-21 09:24:33 +0600

    changes

--- src/OOP/lab/Online2/MyCalculator.java
+++ src/OOP/lab/Online2/MyCalculator.java
@@ -13,12 +13,5 @@ public class MyCalculator implements Calculator{
         return fact;
     }
 
-    public static void main(String[] args) {
-        System.out.print("Enter a number: ");
-        Scanner sc = new Scanner(System.in);
-        int input = sc.nextInt();
-        MyCalculator calc = new MyCalculator();
-        int output= calc.factorial(input);
-        System.out.println("Factorial is: " + output);
-    }
+
 }
